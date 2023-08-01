832374f20a61d5306bdfc1b1e3a25de627b7be15
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-08-01 21:10:58 +0400

    Добавлен класс Product

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineStore
+{
+    public class Product
+    {
+        public Product(string title, decimal price)
+        {
+            if (string.IsNullOrEmpty(title)) throw new ArgumentException(nameof(title));
+            if (price <= 0) throw new ArgumentException($"Цена задана некорректно! {nameof(price)}");
+
+            Title = title;
+            Price = price;
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
