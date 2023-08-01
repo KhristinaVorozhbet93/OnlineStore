534e3997e66e3584631d0eca576c82b037ae09e7
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-08-01 22:47:45 +0400

    Вынесла ProductItem компонент в отдельную папку

---
+++ Components/ProductItem.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OnlineStore.Components
+{
+    public partial class ProductItem
+    {
+        [Parameter]
+        public Product Product { get; set; }
+        [Inject]
+        public NavigationManager Manager { get; set; }
+
+        public void ToProductInfoPage()
+        {
+            Manager.NavigateTo($"products/{Product.Id}");
+        }
+    }
+}
