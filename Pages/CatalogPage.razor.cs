10d0c371ae21ec6c97f60cac63d544ec5f094dbc
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-08-01 22:35:19 +0400

    Разделила код и UI по разным файлам в страницах CatalogPage и ProductInfoPage

---
+++ Pages/CatalogPage.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OnlineStore.Pages
+{
+    public partial class CatalogPage
+    {
+        [Inject]
+        public ICatalog Catalog { get; set; }
+        private List<Product> _products;
+
+        protected override void OnInitialized()
+        {
+            _products = Catalog.GetProducts();
+        }
+    }
+}
---
+++ Pages/ProductInfoPage.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OnlineStore.Pages 
+{
+    public partial class ProductInfoPage
+    {
+        [Inject]
+        public ICatalog Catalog { get; set; }
+        [Parameter]
+        public Guid ProductId { get; set; }
+
+        private Product _product;
+        protected override void OnInitialized()
+        {
+            _product = Catalog.GetProduct(ProductId);
+        }
+    }
+}
