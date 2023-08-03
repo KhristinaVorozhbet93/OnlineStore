d5725f54d59c7fef62bb4df0d77b95107b044ce1
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-08-03 17:21:50 +0400

    Добавлен проект по Backend

---
+++ OnlineStore.Backend/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineStore.Backend
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Product> Products => Set<Product>(); 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+    }
+}
