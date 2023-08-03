3ebc42d1eebe0fac2d3c2292d9daae295f76a47a
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-08-03 17:30:55 +0400

    Добавлен класс Product в Backend

---
+++ OnlineStore.Backend/Data/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStore.Backend.Data
+{
+    public class Product
+    {
+        public Guid Id { get; init; }
+        public string Title { get; set; }
+        public string Price { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
