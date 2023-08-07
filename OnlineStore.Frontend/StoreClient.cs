18bb96fbca49afd31873b71d7a3d86122b56027a
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-08-07 23:00:06 +0400

    Изменена реализация интерфейса IDisposable

--- OnlineStore.Frontend/StoreClient.cs
+++ OnlineStore.Frontend/StoreClient.cs
@@ -6,16 +6,27 @@ namespace OnlineStore
     {
         private readonly string _host; 
         private readonly HttpClient? _httpClient;
+        private bool _disposed;
 
-        public StoreClient(string host = "http://mystore.com/",HttpClient? client = null)
+        public StoreClient(string host = "http://mystore.com/", HttpClient? client = null)
         {
             if (string.IsNullOrEmpty(host)) throw new ArgumentException(nameof(host));
             if (!Uri.TryCreate(host, UriKind.Absolute, out var hostUri))
             {
                 throw new ArgumentException("The host adress shoild be a valid URL", nameof(host));
             }
-            _host = host; 
-            _httpClient = client ?? new HttpClient();
+            _host = host;
+            
+            if (client is null)
+            {
+                _httpClient = new HttpClient();
+                _disposed = true;
+            }
+            else
+            {
+                _httpClient = client;
+            }
+
             if (_httpClient.BaseAddress is null)
             {
                 _httpClient.BaseAddress = hostUri;
@@ -23,31 +34,44 @@ namespace OnlineStore
         }
         public void Dispose()
         {
-            _httpClient.Dispose();
+            if (_disposed)
+            {
+                _httpClient!.Dispose();
+            }
         }
         public async Task<List<Product>> GetProducts()
         {
             var uri = "get_products";
             var products = await _httpClient!.GetFromJsonAsync<List<Product>>(uri);
+            if (products is null)
+            {
+                throw new ArgumentNullException(nameof(products));
+            }
             return products;
         }
         public async Task<Product> GetProduct()
         {
             var uri = "get_product";
-            var products = await _httpClient!.GetFromJsonAsync<Product>(uri);
-            return products;
+            var product = await _httpClient!.GetFromJsonAsync<Product>(uri);
+            if (product is null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+            return product;
         }
         public async Task AddProduct(Product product)
         {
             ArgumentNullException.ThrowIfNull(product);
             var uri = "add_product";
-            await _httpClient!.PostAsJsonAsync(uri, product);
+            var response = await _httpClient!.PostAsJsonAsync(uri, product);
+            response.EnsureSuccessStatusCode(); 
         }
         public async Task DeleteProduct(Product product)
         {
             ArgumentNullException.ThrowIfNull(product);
             var uri = "delete_product";
-            await _httpClient!.PostAsJsonAsync(uri, product);
+            var response = await _httpClient!.PostAsJsonAsync(uri, product);
+            response.EnsureSuccessStatusCode();
         }
     }
 }
